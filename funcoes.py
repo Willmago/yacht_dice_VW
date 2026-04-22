067f580fdd8f7500adbc5ed181f74ab4b8930e2b
Willmago <willneves23607@gmail.com>
2026-04-22 18:15:01 -0300

    Função: guardar dado

--- funcoes.py
+++ funcoes.py
@@ -11,3 +11,19 @@ def rolar_dados(n):
         i += 1
     
     return lista
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+
+    dado_que_eu_quero = dados_rolados[dado_para_guardar]
+
+    dados_no_estoque.append(dado_que_eu_quero)
+
+    dados_rolados_novo = []
+
+    retirado = False
+    for i in range(len(dados_rolados)):
+
+        if i != dado_para_guardar:
+            dados_rolados_novo.append(dados_rolados[i])
+
+    return [dados_rolados_novo, dados_no_estoque]
