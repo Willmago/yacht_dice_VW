1cf4823cac71164c5251acd3b42eae43df0c9bec
Willmago <willneves23607@gmail.com>
2026-04-28 15:05:25 -0300

    Função: calcula sequencia alta

--- funcoes.py
+++ funcoes.py
@@ -72,3 +72,11 @@ def calcula_pontos_sequencia_baixa(dados):
             return 15
     
     return 0
+
+def calcula_pontos_sequencia_alta(dados):
+
+    if 2 in dados and 3 in dados and 4 in dados and 5 in dados:
+        if 1 in dados or 6 in dados:
+            return 30
+    
+    return 0
